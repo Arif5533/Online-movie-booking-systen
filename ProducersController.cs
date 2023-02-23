9e0d1383df38312b534579a77647fad33cbbdb7d
Arif5533 <69283186+Arif5533@users.noreply.github.com>
2023-02-23 20:34:25 +0600

    Add files via upload

---
+++ ProducersController.cs
@@ -0,0 +1,45 @@
+﻿using imovie.Data;
+using imovie.Data.Services;
+using imovie.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace imovie.Controllers
+{
+    public class ProducersController : Controller
+    {
+
+        private readonly IProducersService _service;
+        public ProducersController(IProducersService service)
+        {
+            _service = service;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+
+            var allProducers = await _service.GetAllAsync();
+            return View(allProducers);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePictureURL,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+
+            await _service.AddAsync(producer);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
